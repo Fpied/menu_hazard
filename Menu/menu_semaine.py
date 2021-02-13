303981f59624e62cf63a67f39e93ee4d43ee65b2
Fpied <piedfrancois@laposte.net>
2021-02-13 15:03:22 +0100

    Optimisation du code sur le menu de la semaine. Ajout d'autre recette de cuisine entrée

--- Menu/menu_semaine.py
+++ Menu/menu_semaine.py
@@ -1,58 +1,62 @@
 import os
 import random
+from glob import glob
+import shutil
 
-entree = ["Salade_de_thon_banane_avocat", "salade_riz_thon_et_haricots", "soupe_de_legumes", "sushis_de_concombre", "Les_bouchées_au_thon"]
-plat = ["Bavette_Brocolis.txt", "Blancs_de_dinde_a_la_moutarde_a_l_ancienne", "Omelette_aux_pommes_de_terre", "Saute_de_dinde_a_la_moutarde_et_estragon", "Bulgogi", "Le_navarin_d_agneau"]
-dessert = ["Far_breton", "Gateau_au_yaourt", "salade_de_fruits", "Meringue", "Le_choco_fondant"]
-
-
+# import des chemins des dossier:
+chemin_entree = r"C:\Users\piedf\OneDrive\Bureau\developpement_web\python\Liste_de_courses\Menu\entree"
+chemin_plat = r"C:\Users\piedf\OneDrive\Bureau\developpement_web\python\Liste_de_courses\Menu\Main"
+chemin_dessert = r"C:\Users\piedf\OneDrive\Bureau\developpement_web\python\Liste_de_courses\Menu\Dessert"
 
 boucle = True
 
 while boucle == True:
     first_question = input("Voulez-vous préparer à manger ? O/N ")
     if first_question == "O" or first_question == "o":
-        choix_entree = random.choice(entree)
-        choix_plat = random.choice(plat)
-        choix_dessert = random.choice(dessert)
-        print("Nous vous proposons ce choix la :")
-        print(choix_entree)
-        print(choix_plat)
-        print(choix_dessert)
-        Voir_recette = input("Voulez-vous voir les recettes ? O/N ")
-        if Voir_recette == "O" or Voir_recette == "o":
-            # Recette entree
-            if choix_entree:
-                chemin_entree = "C:\\Users\\piedf\\OneDrive\\Bureau\\developpement_web\\python\\Liste_de_courses\\Menu\\entree\\"
-                # print("Salade de thon banane avocat")
-                lol_entree = chemin_entree + choix_entree + ".txt"
-                with open(lol_entree, "r", encoding="utf-8") as f:
-                    contenu_entree = f.read()
-                    print(contenu_entree)
-            # Recette plat principale
-            if choix_plat:
-                chemin_plat = "C:\\Users\\piedf\\OneDrive\\Bureau\\developpement_web\\python\\Liste_de_courses\\Menu\\Main\\"
-                # print("Bavette Brocolis")
-                lol_plat = chemin_plat + choix_plat + ".txt"
-                with open(lol_plat, "r", encoding="utf-8") as f:
-                    contenu_plat = f.read()
-                    print(contenu_plat)
-            # recette dessert
-            if choix_dessert:
-                chemin_dessert = "C:\\Users\\piedf\\OneDrive\\Bureau\\developpement_web\\python\\Liste_de_courses\\Menu\\Dessert\\"
-                # print("Far Breton")
-                lol_dessert = chemin_dessert + choix_dessert + ".txt"
-                with open(lol_dessert, "r", encoding="utf-8") as f:
-                    contenu_dessert = f.read()
-                    print(contenu_dessert)
+        # choix de l'entree:
+        fichiers = glob(os.path.join(chemin_entree, "*"))
+        choix_entree = random.choice(fichiers)
+        nom_entree = os.path.basename(choix_entree)
+        print(nom_entree)
+       
+        # choix du plat principale:
+        fichiers_plat = glob(os.path.join(chemin_plat, "*"))
+        choix_plat = random.choice(fichiers_plat)
+        nom_plat = os.path.basename(choix_plat)
+        print(nom_plat)
+        
+        # choix du dessert:
+        fichiers_dessert = glob(os.path.join(chemin_dessert, "*"))
+        choix_dessert = random.choice(fichiers_dessert)
+        nom_dessert = os.path.basename(choix_dessert)
+        print(nom_dessert)
+        
+        second_question = input("Voulez vous voir les recettes ? O/N ")
+        if second_question == "O" or second_question == "o":
+            # affichage de la recette entrée
+            with open(choix_entree, "r", encoding="utf-8") as f:
+                contenu_entree = f.read()
+                print(contenu_entree)
+            # affichage du plat principale
+            with open(choix_plat, "r", encoding="utf-8") as f:
+                contenu_plat = f.read()
+                print(contenu_plat)
+            #  affichage du dessert
+            with open(choix_dessert, "r", encoding="utf-8") as f:
+                contenu_dessert = f.read()
+                print(contenu_dessert)
         else:
-            print("sortir de la boucle")
             boucle = False
-            
-
+            print("sorti de la boucle")
     else:
-        print("sortir de la boucle")
         boucle = False
+        print("sorti de la boucle")
+
+
+
+        
+        
+           
     
 
 
--- Menu/test.py
+++ Menu/test.py
@@ -1,5 +1,15 @@
 import os
+from glob import glob
+import shutil
+import random
 
-with open("Dessert/far_breton.txt", "r") as g and open("Dessert/Gateau_au_yaourt.py", "r") as f:
-    content = f.read() + g.read()
-    print(content)
+# chemin de mes entrée
+chemin_entree = r"C:\Users\piedf\OneDrive\Bureau\developpement_web\python\Liste_de_courses\Menu\entree"
+
+fichiers = glob(os.path.join(chemin_entree, "*"))
+choix_entree = random.choice(fichiers)
+
+# Lire le fichier choisi
+with open(choix_entree, "r", encoding="utf-8") as f:
+    contenu_entree = f.read()
+    print(contenu_entree)
